65ca9e31ad8651ace5db28835ecdbd6a559a4295
Cristian Danilo Gutiérrez Angarita <cristian.gutierrez@Cristian-Gutierrez-MacBook-Pro.local>
2022-08-23 16:47:16 -0500

    all done

--- src/authenticated-app.exercise.js
+++ src/authenticated-app.exercise.js
@@ -2,15 +2,10 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// We'll be doing a lot of stuff with the router on this page.
-// 🐨 Here's what you'll need to import from react-router-dom
-// Routes, Route, Link
 import {Routes, Route, Link, useMatch} from 'react-router-dom'
 import {Button} from './components/lib'
 import * as mq from './styles/media-queries'
 import * as colors from './styles/colors'
-// 🐨 you'll need to import all the screen components in the screens directory
-// 💰 DiscoverBooksScreen, BookScreen, NotFoundScreen
 import {DiscoverBooksScreen} from './screens/discover'
 import {BookScreen} from './screens/book.exercise'
 import {NotFoundScreen} from './screens/not-found.exercise'
@@ -61,7 +56,6 @@ function AuthenticatedApp({user, logout}) {
 
 function NavLink(props) {
   const matches = useMatch(props.to)
-  // 🐨 change this from an <a /> to a <Link />
   return (
     <Link
       css={[
@@ -115,10 +109,6 @@ function Nav() {
         }}
       >
         <li>
-          {/*
-              🐨 Once the NavLink has been updated to use a Router Link,
-                change from the href prop to a "to" prop
-          */}
           <NavLink to="/discover">Discover</NavLink>
         </li>
       </ul>
@@ -127,13 +117,6 @@ function Nav() {
 }
 
 function AppRoutes({user}) {
-  // 🐨 Return all the routes here.
-  // 💰 Here's the mapping of URL to element:
-  //     /discover         <DiscoverBooksScreen user={user} />
-  //     /book/:bookId     <BookScreen user={user} />
-  //     *                 <NotFoundScreen />
-  //
-  // Make sure to check the INSTRUCTIONS.md for how this should be structured
   return <Routes>
       <Route path="/discover" element={<DiscoverBooksScreen user={user}/>}/>
       <Route path="/book/:bookId" element={<BookScreen user={user}/>}/>
--- src/components/book-row.exercise.js
+++ src/components/book-row.exercise.js
@@ -1,8 +1,5 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
-
-// We're going to turn the entire book row into a link to the book page
-// 🐨 import the Link component from react-router-dom
 import {Link} from 'react-router-dom'
 import * as mq from 'styles/media-queries'
 import * as colors from 'styles/colors'
@@ -21,10 +18,6 @@ function BookRow({book}) {
         position: 'relative',
       }}
     >
-      {/*
-          🐨 Turn this div into a Link
-          and add a to prop to make it direct to `/book/${book.id}`
-      */}
       <Link
         to={`/book/${book.id}`}
         aria-labelledby={id}
