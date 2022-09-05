51de7c7788822b86c9d73792811e40e8b65cc3f2
Cristian Danilo Gutiérrez Angarita <cristian.gutierrez@Cristian-Gutierrez-MacBook-Pro.local>
2022-09-05 12:10:32 -0500

    all done

--- src/components/profiler.exercise.js
+++ src/components/profiler.exercise.js
@@ -1,1 +1,46 @@
-// this is for extra credit
+import React from 'react';
+import { client } from 'utils/api-client';
+
+let queue = [];
+
+setInterval(sendProfileQueue, 5000);
+
+function sendProfileQueue() {
+  if (!queue.length) {
+    return Promise.resolve({ success: true });
+  }
+
+  const queueToSend = [...queue];
+  queue = [];
+
+  return client('profile', { data: queueToSend });
+}
+
+function Profiler({ id, phases, metadata, ...props }) {
+  function reportProfile(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions
+  ) {
+    if (!phases || phases.includes(phase)) {
+      queue.push({
+        id,
+        phase,
+        metadata,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions
+      });
+    }
+  }
+
+  return <React.Profiler id={id} onRender={reportProfile} {...props} />;
+}
+
+export { Profiler };
